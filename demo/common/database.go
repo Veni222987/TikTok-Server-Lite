fdebe5863b27fcacd8118d46923708c6c89e4f3e
HAHA <110449952+fairy12181210@users.noreply.github.com>
2023-08-12 03:07:50 +0800

    Add files via upload

---
+++ demo/common/database.go
@@ -0,0 +1,39 @@
+package common
+
+import (
+	"fmt"
+	"ginEssential/model"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() *gorm.DB {
+	host := "localhost"
+	port := "3306"
+	database := "dome"
+	username := "root"
+	password := "1234"
+	charset := "utf8"
+	args := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=True&loc=Local",
+		username,
+		password,
+		host,
+		port,
+		database,
+		charset)
+	db, err := gorm.Open(mysql.Open(args), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database,err: " + err.Error())
+	}
+
+	db.AutoMigrate(&model.User{})
+
+	DB = db
+	return db
+}
+
+func GetDB() *gorm.DB {
+	return DB
+}
